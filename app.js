9e3ed2f9615c4c1afaf042441b9e4d3b52b7920f
Abdullah <sythertechnology@gmail.com>
2023-06-13 18:07:49 +0500

    completed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // MongoDB configuration
-mongoose.connect('mongodb://192.168.18.133:27017/projective_tests', {
+mongoose.connect('mongodb://192.168.1.6:27017/projective_tests', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -39,24 +39,15 @@ const upload = multer({
     },
 });
 
-// Upload a test result
-app.post('/api/test-results', async (req, res) => {
+app.post('/predict', upload.single('image'), async (req, res) => {
     try {
-        const { testName, imageData } = req.body;
+        const imageData = req.file.buffer; // Assuming the image data is sent in the 'imageData' field of the request body
 
-        const processedImageData = await processImageData(imageData);
-
-        const testResult = new TestResult({
-            testName,
-            imageData: processedImageData,
-        });
-
-        await testResult.save();
-
-        res.status(201).json({ message: 'Test result saved successfully' });
+        const predictedLabel = await predictImageData(imageData);
+        res.json({ label: predictedLabel });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to save test result' });
+        console.error('Error predicting image:', error);
+        res.status(500).json({ error: 'Error predicting image' });
     }
 });
 
@@ -79,9 +70,9 @@ app.post('/api/train-data', upload.array('images'), async (req, res) => {
 
         for (let i = 0; i < images.length; i++) {
             const imageData = images[i].buffer;
-            const label = req.body.labels[i]; // Assuming the labels are sent in the same order as the images
+            const label = req.body.label; // Assuming the labels are sent in the same order as the images
 
-            const processedImageData = await processImageData(imageData);
+            const processedImageData = await processUploadData(imageData);
 
             const trainData = new TrainData({
                 imageData: processedImageData,
@@ -98,82 +89,146 @@ app.post('/api/train-data', upload.array('images'), async (req, res) => {
     }
 });
 
+let inputFeatureSize;
+
 // Train the Naive Bayesian model
 async function trainModel() {
     try {
         const trainData = await TrainData.find();
-        const trainDataset = [];
 
-        trainData.forEach((data) => {
-            const inputFeature = processImageData(data.imageData);
-            const label = data.label;
-
-            trainDataset.push({ inputFeature, label });
-        });
-
-        if (trainDataset.length === 0) {
+        if (trainData.length === 0) {
             console.log('No training data available');
             return;
         }
 
-        const inputFeatureSize = trainDataset[0].inputFeature.length;
-        const labelSize = [...new Set(trainDataset.map((data) => data.label))].length;
+        const uniqueLabels = [...new Set(trainData.map((data) => data.label))];
+        const labelSize = uniqueLabels.length;
 
-        const model = tf.sequential();
-        model.add(tf.layers.dense({ units: 10, inputShape: [inputFeatureSize] }));
-        model.add(tf.layers.dense({ units: labelSize, activation: 'softmax' }));
-        model.compile({ loss: 'categoricalCrossentropy', optimizer: 'adam' });
+        if (labelSize < 2) {
+            console.log('Insufficient number of unique labels');
+            return;
+        }
 
-        const xsTrain = tf.tensor2d(
-            trainDataset.map((data) => data.inputFeature)
-        );
-        const ysTrain = tf.tensor2d(
-            trainDataset.map((data) => oneHotEncodeLabel(data.label, labelSize))
-        );
-
-        await model.fit(xsTrain, ysTrain, {
-            epochs: 10,
-            validationSplit: 0.2,
-            shuffle: true,
+        const trainDataset = trainData.map(async (data) => {
+            const inputFeature = await processImageData(data.imageData);
+            const label = data.label;
+            return { inputFeature, label };
         });
 
-        const evaluation = model.evaluate(xsTrain, ysTrain);
+        // Wait for all image processing to complete
+        const processedDataset = await Promise.all(trainDataset);
 
-        console.log(`Training accuracy: ${evaluation[1].dataSync()[0]}`);
+        if (processedDataset.length === 0) {
+            console.log('Empty training dataset');
+            return;
+        }
 
-        await model.save('./saveModel');
+        inputFeatureSize = processedDataset[0].inputFeature.length;
 
-        console.log('Model training completed');
+        const xsTrain = tf.tensor2d(
+            processedDataset.map((data) => {
+                const inputFeature = Array.isArray(data.inputFeature) ? data.inputFeature : [data.inputFeature];
+                return inputFeature.flat();
+            }),
+            [processedDataset.length, inputFeatureSize]
+        );
+
+        const ysTrain = tf.oneHot(
+            processedDataset.map((data) => uniqueLabels.indexOf(data.label)),
+            labelSize
+        );
+
+        // Define the model architecture
+        const model = tf.sequential();
+        model.add(tf.layers.dense({ units: 16, activation: 'relu', inputShape: [inputFeatureSize] }));
+        model.add(tf.layers.dense({ units: labelSize, activation: 'softmax' }));
+
+        // Compile the model
+        model.compile({ loss: 'categoricalCrossentropy', optimizer: 'adam', metrics: ['accuracy'] });
+
+        // Train the model
+        await model.fit(xsTrain, ysTrain, {
+            epochs: 25,
+            batchSize: 32,
+        });
+
+        console.log('Model trained successfully');
+        await model.save('file://./saveModel');
+        console.log('Model saved successfully');
     } catch (error) {
         console.error('Error during model training:', error);
     }
 }
 
-// Utility function to one-hot encode labels
-function oneHotEncodeLabel(label, labelSize) {
-    const encodedLabel = new Array(labelSize).fill(0);
-    encodedLabel[label] = 1;
-    return encodedLabel;
-}
-
-// Utility function to process image data
+// // // Function to process image data using Jimp
 async function processImageData(imageData) {
     try {
         const image = await Jimp.read(imageData);
         const processedImage = image.resize(256, 256).quality(90);
 
-        return processedImage.getBufferAsync(Jimp.MIME_JPEG);
+        const pixelData = Array.from(processedImage.bitmap.data);
+        return pixelData;
+    } catch (error) {
+        console.error('Error processing image data:', error);
+        throw error;
+    }
+}
+async function processUploadData(imageData) {
+    try {
+        // Load the image using Jimp
+        const image = await Jimp.read(Buffer.from(imageData));
+
+        // Resize and perform other necessary image processing operations
+        const processedImage = image.resize(256, 256).greyscale();
+
+        // Convert the processed image to a buffer or another suitable format
+        const processedImageData = await processedImage.getBufferAsync(Jimp.MIME_JPEG);
+
+        return processedImageData;
     } catch (error) {
         console.error('Error processing image data:', error);
         throw error;
     }
 }
 
-// Train the model when the server starts
-trainModel();
 
-// Start the server
-const PORT = process.env.PORT || 3000;
+async function predictImageData(imageData) {
+    try {
+        const processedImage = await processImageData(imageData);
+        const inputFeature = Array.isArray(processedImage) ? processedImage : [processedImage];
+
+        // Retrieve the input feature size from the saved model
+        const model = await tf.loadLayersModel('file://./saveModel/model.json');
+        const inputFeatureSize = model.inputs[0].shape[1];
+
+        const inputTensor = tf.tensor2d([inputFeature], [1, inputFeatureSize]);
+
+        const prediction = model.predict(inputTensor);
+        const predictedLabelIndex = tf.argMax(prediction, 1).dataSync()[0];
+
+        const trainData = await TrainData.find();
+
+        if (trainData.length === 0) {
+            console.log('No training data available');
+            return;
+        }
+
+        const uniqueLabels = [...new Set(trainData.map((data) => data.label))];
+        console.log(uniqueLabels)
+        const labels = uniqueLabels.length > 0 ? uniqueLabels : ['Person Under The Rain', 'Family', 'Human Figure']; // Replace with your own labels
+        const predictedLabel = labels[predictedLabelIndex];
+
+        return predictedLabel;
+    } catch (error) {
+        console.error('Error predicting image data:', error);
+        throw error;
+    }
+}
+app.get('/trainData', (req, res) => {
+    trainModel();
+})// Call this function to train the model during application startup
+
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
